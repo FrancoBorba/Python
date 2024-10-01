0e58e9d2449bced365f0207fd2c043541b0a767d
FrancoBorba <franco.borba14@gmail.com>
2024-10-01 15:41:37 -0300

    Aula 02 do curso sobre sintaxe em python

---
+++ sintaxeBasica.py
@@ -0,0 +1,21 @@
+#Arquivo referente a aula 2 do curso de python do CBF curso 
+
+num = 1 #criando variavel , a variavel não possui um tipo
+print(num)
+num = " bruno" # a variavel virou uma string pois recebeu uma string
+print(num)
+
+numero1 = 5
+numero2 = 10 
+numero3 = numero2 + numero1
+
+print(numero3)
+if 10 > 2:
+ print("Para printar o comando tem que esta no escopo do if")
+ print("Dentro do escopo")
+print("Fora do escopo")
+
+if 10 < 2:
+ print("Para printar o comando tem que esta no escopo do if")
+ print("Dentro do escopo")
+print("Fora do escopo")
