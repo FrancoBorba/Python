a2078500c3c05e7d2a81c546048061b036a414af
FrancoBorba <franco.borba14@gmail.com>
2024-10-09 14:32:39 -0300

    Aulas sobre boolean

---
+++ boolean.py
@@ -0,0 +1,16 @@
+# variaveis boolean são aquelas que tem apenas dois valores , verdadeiro e falso
+
+x = True
+y = False
+
+print(x)
+print(y)
+
+# operadores logicos 
+
+# and ( os dois devem ser verdadeiro)
+# or ( apenas um precisa ser verdadeiro)
+# not ( negacao do atual)
+
+print(x and y)
+print(x or y) 
